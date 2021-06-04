740fc60ad22213f71e07cc103c7902d6e2b414d2
Jeffrey Lanters <jeffreylanters@me.com>
2021-06-04 15:21:43 +0200

    Added Class for wrapping JSON Arrays

---
+++ Runtime/Core/JsonArrayWrapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace JeffreyLanters.WebRequests.Core {
+
+  /// <summary>
+  /// A class to wrap top level JSON arrays. When the Model type is a top level
+  /// array, it will be wrapped into this empty model in order for Unity to
+  /// parse it contents since top level arrays are not supported in Unity.
+  /// </summary>
+  /// <typeparam name="ArrayType">Type of the data.</typeparam>
+  [Serializable]
+  public class JsonArrayWrapper<ArrayType> {
+
+    /// <summary>
+    /// The data that should be wrapped.
+    /// </summary>
+    public ArrayType array = default;
+  }
+}
