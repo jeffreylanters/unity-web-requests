8893f5074c2bc4f36fcc00e9dbaab4d21b617c1e
GamezDanWork <contact@gamerzdan.com>
2023-01-08 22:43:43 +0530

    Add StatusCode, headers and error message fields to WebRequestResponse Add helper method to convert String dictionary into QueryParameters string

--- Runtime/Core/WebRequestResponse.cs
+++ Runtime/Core/WebRequestResponse.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine.Networking;
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace JeffreyLanters.WebRequests.Core {
 
@@ -15,13 +16,28 @@ namespace JeffreyLanters.WebRequests.Core {
     /// </summary>
     public string webRequestResponseText { get; private set; } = "";
 
+    public string ErrorMessage { get; private set; } = "";
+
+    /// <summary>
+    /// The response status code from the web request.
+    /// </summary>
+    public long StatusCode { get; private set; }
+
+    /// <summary>
+    /// The response headers from the web request.
+    /// </summary>
+    public Dictionary<string, string> headers { get; private set; }
+
     /// <summary>
     /// Instanciates a new web request response based of off a web request
     /// handler. The required data will be extracted from the download handler.
     /// </summary>
     /// <param name="webRequestHandler"></param>
     public WebRequestResponse (WebRequestHandler webRequestHandler) {
-      this.webRequestResponseText = webRequestHandler.downloadHandler.text;
+            this.webRequestResponseText = webRequestHandler.downloadHandler.text;
+            this.StatusCode = webRequestHandler.responseCode;
+            this.headers = webRequestHandler.GetResponseHeaders();
+            this.ErrorMessage = webRequestHandler.error;
     }
 
     /// <summary>
@@ -32,13 +48,19 @@ namespace JeffreyLanters.WebRequests.Core {
       return this.webRequestResponseText;
     }
 
-    /// <summary>
-    /// Converts the Web Request's response text as a JSON Objects to a given
-    /// data type.
-    /// </summary>
-    /// <typeparam name="DataType">The Type to convert the JSON to.</typeparam>
-    /// <returns>The Object</returns>
-    public DataType Json<DataType> () where DataType : class {
+    public string Content { get
+            {
+                return this.webRequestResponseText;
+            }
+        }
+
+        /// <summary>
+        /// Converts the Web Request's response text as a JSON Objects to a given
+        /// data type.
+        /// </summary>
+        /// <typeparam name="DataType">The Type to convert the JSON to.</typeparam>
+        /// <returns>The Object</returns>
+        public DataType Json<DataType> () where DataType : class {
       return typeof (DataType).IsArray ?
         JsonUtility.FromJson<JsonArrayWrapper<DataType>> ($"{{\"array\":{this.webRequestResponseText}}}").array :
         JsonUtility.FromJson<DataType> (this.webRequestResponseText);
